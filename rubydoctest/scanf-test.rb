af5046eb0bba662371fbf563e367666c7e93eed8
Douglas G. Allen <kb9agt@gmail.com>
2015-08-27 04:00:32 -0500

    Create scanf-test.rb

---
+++ rubydoctest/scanf-test.rb
@@ -0,0 +1,10 @@
+=begin
+doctest: str.scanf("%d%s") returns [[246, "ABC"], [912, "DEF"], [1578, "GHI"]]
+>> str.scanf("%d%s") { |num,str| [ num * 2, str.upcase ] }
+=> [[246, "ABC"], [912, "DEF"], [1578, "GHI"]]
+=end
+
+require 'scanf'
+
+str = "123 abc 456 def 789 ghi"
+str.scanf("%d%s") { |num,str| [ num * 2, str.upcase ] }
